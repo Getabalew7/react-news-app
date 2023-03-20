1d8e35da774fdce0c80d0c35197e1b698b0932ca
getabalew7 <getabalew7@gmail.com>
2023-03-20 18:28:29 +0100

    add comment to the NewsService class

--- src/service/NewsService.js
+++ src/service/NewsService.js
@@ -1,5 +1,6 @@
 import axios  from "axios";
-const API_KEY="d6d94562b44e4e21baaaacc3691fd2f2";
+
+const API_KEY="d6d94562b44e4e21baaaacc3691fd2f2"; // The key should not be stored here.
 //const API_KEY="9672383d39e04694a8536059d227903d";
 const DEFAULT_NEWS_URL ="https://newsapi.org/v2/top-headlines?country=us&category=business";
 class NewsService {
